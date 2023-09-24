70ff1865774fba47951cdca0d339a6a5622e01a5
Priti Basak <75367921+pritibasak@users.noreply.github.com>
2023-09-24 17:20:42 +0530

    After configuring tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //this array of regEx helps to add taiwind css in all types of files mentioned as extension 
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
